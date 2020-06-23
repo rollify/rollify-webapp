7e347e4f1a581496950c0dbd423d66822b25b016
Xabier Larrakoetxea <me@slok.dev>
2020-06-23 09:34:29 +0200

    Add qinput and remove badge from die component button
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- src/components/DieRollBtn.vue
+++ src/components/DieRollBtn.vue
@@ -1,23 +1,31 @@
 <template>
-  <div>
-    <q-btn
-      round
-      unelevated
-      size="50px"
-      dense
-      :icon="'img:' + die.metadata.buttonImage"
-      v-model="die"
-      @click="incDiceBag()"
-    >
-      <q-badge
-        v-if="die.quantity > 0"
-        :color="die.metadata.color"
-        floating
-        v-model="die"
-      >
-        {{ die.quantity }}
-      </q-badge>
-    </q-btn>
+  <div class="row">
+    <div class="col">
+      <div class="row">
+        <q-btn
+          round
+          unelevated
+          size="50px"
+          dense
+          :icon="'img:' + die.metadata.buttonImage"
+          v-model="die"
+          @click="incDiceBag()"
+        />
+      </div>
+      <div class="row justify-center">
+        <q-input
+          dense
+          v-model="die"
+          v-model.number="die.quantity"
+          :color="die.metadata.color"
+          :label-color="die.metadata.color"
+          type="number"
+          outlined=""
+          standout
+          style="max-width: 75px"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
